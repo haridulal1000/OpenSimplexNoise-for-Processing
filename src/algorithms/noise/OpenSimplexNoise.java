a2f6e80c3020d49cd8aa07b00f780a877d6e7d7b
Hari Dulal <84311145+haridulal1000@users.noreply.github.com>
2022-02-26 23:04:32 +0545

    Update OpenSimplexNoise.java

--- src/algorithms/noise/OpenSimplexNoise.java
+++ src/algorithms/noise/OpenSimplexNoise.java
@@ -5,10 +5,6 @@ package algorithms.noise;
  * This is a template class and can be used to start a new processing Library.
  * Make sure you rename this class as well as the name of the example package 'template' 
  * to your own Library naming convention.
- * 
- * (the tag example followed by the name of an example included in folder 'examples' will
- * automatically include the example in the javadoc.)
- *
  * @example Hello 
  */
 public class OpenSimplexNoise {
